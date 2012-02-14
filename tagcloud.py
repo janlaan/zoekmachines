a8085f93b77ddda30778b01d0ad4a7359680a445
Jan <laanjj@gmail.com>
2012-02-14 12:55:36 +0100

    aanpassigen

--- tagcloud.py
+++ tagcloud.py
@@ -6,14 +6,10 @@ def print_cloud(words):
     
 def make_cloud(words):
   sorted_words = sorted(words.iteritems(), key=operator.itemgetter(1))
-  pprint(sorted_words.__class__)
+  
   sorted_words.reverse()
   output = ''
-  pprint(words)
-  pprint(sorted_words)
   for i in range(0,10):
-    pprint(sorted_words[i])
-    pprint(sorted_words[i][0])
-    output += '<span style="font-size: ' + str(sorted_words[i][1]) +'em;">' + sorted_words[i][0] + "</span>"
+    output += '<span style="font-size: ' + str(sorted_words[i][1]) +'em;">' + sorted_words[i][0] + "</span>  &nbsp;&nbsp; "
   
   return output
