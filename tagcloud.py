45e6729874822a50911f8b90e38aa8c1656cc701
Michael Faber <michaelf@u019925.science.uva.nl>
2012-02-23 14:27:24 +0100

    update woordenwolk in site

--- tagcloud.py
+++ tagcloud.py
@@ -1,17 +1,15 @@
 import operator
 from pprint import pprint
-def print_cloud(words):
-  for w in words:
-    print w
+from functions import *
+from math import log10
     
-def make_cloud(words):
-  sorted_words = sorted(words.iteritems(), key=operator.itemgetter(1))
+def make_cloud(docid, searcher):
+  sorted_words = get_keywords(docid, searcher)
   
-  sorted_words.reverse()
   output = '<form action="http://www.wordle.net/advanced" method="POST"> <textarea name="wordcounts" style="display:none">'
 
   for i in range(0,10):
-    output += sorted_words[i][0] +':'+ str(sorted_words[i][1])
+    output += sorted_words[i][0] +':'+ str(log10(sorted_words[i][1])) + '\n' 
     
   output += '</textarea> <input type="submit" value="Woordenwolk"> </form>'
   return output
